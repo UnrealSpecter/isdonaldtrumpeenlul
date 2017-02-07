397327246504059231abc2d8ec1f05e61dab6fa9
Tomi Ristic <tomi_emmen@hotmail.com>
2017-02-07 02:59:31 +0100

    finished this amazing site lul

---
+++ resources/views/trump.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Is hij een lul? Nou best wel he</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            .vertical-center {
+                position: absolute;
+                top: 0; 
+                left: 0; 
+                height: 100vh; 
+                width: 100vw; 
+                line-height: 100vh; 
+                text-align: center; 
+                background: #F37E21; 
+                color: white;
+                padding: 0;
+                margin: 0;
+                font-size: 70vh;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <h1 class="vertical-center">Ja.</h1>
+            </div>
+        </div>
+    </body>
+</html>
+
