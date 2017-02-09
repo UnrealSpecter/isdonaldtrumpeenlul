0819be6acbf9b2f618d83e4ebc0d9495a419f655
Tomi Ristic <tomi_emmen@hotmail.com>
2017-02-09 14:58:33 +0100

    added footer, link to dhevak facebook and better font scaling.

--- resources/views/trump.blade.php
+++ resources/views/trump.blade.php
@@ -4,9 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Is hij een lul? Nou best wel he</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <title>Donald Trump is een lul</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <style type="text/css">
             .vertical-center {
@@ -23,12 +21,47 @@
                 margin: 0;
                 font-size: 25vw;
             }
+
+            .footer {
+                position: fixed;
+                bottom: 0;
+                left: 0;
+                height: 5vh;
+                color: white;
+                text-align: center;
+                width: 100vw;
+                font-size: 2.5vw;
+                font-style: italic;
+            }
+
+            a {
+                text-decoration: none;
+                color: white;
+            }
+
+            a:hover {
+                text-decoration: none;
+                color: white;
+            }
         </style>
+        <!-- Google Analytics -->
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-90310031-1', 'auto');
+        ga('send', 'pageview');
+        </script>
+        <!-- End Google Analytics -->
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <h1 class="vertical-center">Ja.</h1>
+                <h1 class="vertical-center">Ja</h1>
+            </div>
+            <div class="footer">
+                <a href="https://www.facebook.com/dhevakart/">by Dhevak *clicky*</a>
             </div>
         </div>
     </body>
